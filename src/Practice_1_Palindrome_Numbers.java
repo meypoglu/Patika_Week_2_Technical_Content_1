4f3fbf48b3cc8d7d62dfbbd50de6d712217af9ce
meyup <m.eypoglu@gmail.com>
2024-05-01 15:34:07 +0300

    Initial commit

---
+++ src/Practice_1_Palindrome_Numbers.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Practice_1_Palindrome_Numbers {
+    public static void main(String[] args) {
+        // Kullanıcıdan girdilerin alınması
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int num = input.nextInt();
+        // Her basamaktaki sayıları sırayla tutan değişken
+        int digit = 0;
+        // Terse çevrilmiş haldeki sayıyı tutacak değişken
+        int reverseNum = 0;
+        // Toplam basamak sayısını bulmak için kullanılan formül
+        int length = (int) Math.log10(num) + 1;
+        // Son basamağın ilk basamak haline gelmesi için 10'un kaç katıyla çarpılması gerektiğini hesaplayan formül
+        double pow10 = Math.pow(10, length - 1);
+        // Orjinal numara değişkeninin sabit kalması için başka bir değişkene değerinin atanması
+        int num2 = num;
+        while (num2 > 0) {
+            // Her döngüde bir sonraki basamaktaki rakam alınır
+            digit = num2 % 10;
+            num2 = num2 / 10;
+            // Her alınan rakam ters haldeki sayıdaki basamağına göre 10'un kuvvetleri ile çarpılır
+            reverseNum += digit * pow10;
+            pow10 = pow10 / 10;
+        }
+        // Eğer verilen sayıyla reverseNum eşit ise palindrom sayı olarak çıktı verilir
+        System.out.println(reverseNum == num ? num + " bir palindrom sayı." : num + " bir palindrom sayı değil.");
+
+    }
+}
